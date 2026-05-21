2a1d5de68ad63305b803e9e78e4d1b4c85f77237
Kumarankit666 <ankitkr816102@gmail.com>
2026-05-21 13:38:25 +0530

    Database Connect

--- .history/backend/database_20260521132841.py
+++ .history/backend/database_20260521132841.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 client = MongoClient(
 
-    "mongodb+srv://resumeiq:GdLy3cZJjOlHC5@resumeiq.sgp8f34.mongodb.net/?retryWrites=true&w=majority&appName=ResumeIQ"
+    "mongodb+srv://resumeiq:Ank816102@@resumeiq.sgp8f34.mongodb.net/?appName=ResumeIQ"
 
 )
 
